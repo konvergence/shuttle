2822ff3564d5bcb6ee3bd9b6a0f3cc2e8499e1a1
Olivier Rochon <olivier.rochon@koeos.lan>
2018-07-31 18:23:41 +0200

    first dockerfile

--- packages/cleanup.sh
+++ packages/cleanup.sh
@@ -6,3 +6,11 @@ rm -rf patches/module/*
 rm -rf patches/server/*
 rm -rf patches/client/*
 rm -rf patches/class/*
+rm -rf documentation/*
+
+touch  installer/empty
+touch patches/module/emtpy
+touch patches/server/emtpy
+touch patches/client/emtpy
+touch patches/class/emtpy
+touch documentation/emtpy
